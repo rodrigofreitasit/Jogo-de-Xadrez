13b02295f25064c6e7ed3a39ea3b6d7607b1d20d
Rodrigo Freitas <rodrigofreitasit@gmail.com>
2018-06-22 19:22:31 -0300

    Comecando implementar o tabuleiro

---
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,14 @@
+﻿namespace tabuleiro {
+    class Tabuleiro {
+
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+        private Peca[,] pecas;
+
+        public Tabuleiro( int linha, int colunas) { // Definir quantas linhas e colunas tera o tabuleiro
+            this.linhas = linhas;
+            this.colunas = colunas;
+            pecas = new Peca[linhas, colunas];   // Criando a matriz de acordo com a quantidade
+        }
+    }
+}
