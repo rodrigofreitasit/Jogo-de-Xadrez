3274501d6fde2dd83d6c5dbf4b577c9b14e391f5
Rodrigo Freitas <rodrigofreitasit@gmail.com>
2018-07-03 19:18:29 -0300

    Melhorando metodo imprimir tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -9,7 +9,7 @@ namespace xadrez_console {
                 Tabuleiro tab = new Tabuleiro(8, 8);
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 9));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 1));
 
                 Tela.imprimirTabuleiro(tab);
             }
--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -7,15 +7,30 @@ namespace xadrez_console {
         public static void imprimirTabuleiro(Tabuleiro tab) {
 
             for (int i = 0; i < tab.linhas; i++) {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.colunas; j++) {
                     if (tab.peca(i, j) == null) {
                         Console.Write("- "); // verifica se tem uma peça caso nao imprime o traço
                     }
                     else {
-                        Console.Write(tab.peca(i, j) + " "); // se tiver peça imprime a preça na posição
+                        imprimirPeca(tab.peca(i, j)); // se tiver peça imprime a preça na posição
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void imprimirPeca(Peca peca) {
+            if (peca.cor == Cor.Branca) {
+                Console.Write(peca);
+            }
+            else {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
